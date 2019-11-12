5cce433a3e24a0081408fddd46103a7a3ac1cd4d
ConsuL <consul.developer@gmail.com>
2019-11-13 01:08:07 +0300

    url fix, msg added

--- backend/views/site/index.php
+++ backend/views/site/index.php
@@ -1,20 +1,24 @@
 <?php
 
+use yii\helpers\Url;
+
 /* @var $this yii\web\View */
 
 $this->title = 'Admin Secure Area';
 ?>
 
 
-<a href="admin/site/movie" class="btn btn-link logout">Add Movie</a>
+<a href="<?= Url::toRoute(['/site/movie']) ?>" class="btn btn-link logout">Add Movie</a>
 <br>
-<a href="admin/site/showtime" class="btn btn-link logout">Add ShowTime</a>
+<a href="<?= Url::toRoute(['/site/showtime']) ?>" class="btn btn-link logout">Add ShowTime</a>
 <br>
 <br><br>
 <h1>ShowTime listing:</h1>
 
 <?php if (!empty($shows)): ?>
 	<?php foreach ($shows as $show): ?>
+
+
         <div class="panel panel-default">
             <div class="panel-heading">
                 <h3 class="panel-title">
@@ -23,12 +27,16 @@ $this->title = 'Admin Secure Area';
 
                 <span style="float: right"><?= date("d-m-Y", $show->date) ?></span>
                 <br>
-                <a href="admin/site/showtime?id=<?= $show->id ?>">[Edit Show]</a>
-                <a href="admin/site/delshow?id=<?= $show->id ?>">[Delete Show]</a>
+                <a href="<?= Url::toRoute(['/site/showtime', 'id' => $show->id]) ?>">[Edit Show]</a>
+                <a onclick="return confirm('Deleting this show?');"
+                   href="<?= Url::toRoute(['/site/delshow', 'id' => $show->id]) ?>">[Delete Show]</a>
+
                 --
-                <a href="admin/site/viewmovie?id=<?= $show->movie_id ?>">[View Movie]</a>
-                <a href="admin/site/movie?id=<?= $show->movie_id ?>">[Edit Movie]</a>
-                <a href="admin/site/delmovie?id=<?= $show->movie_id ?>">[Delete Movie]</a>
+
+                <a href="<?= Url::toRoute(['/site/viewmovie', 'id' => $show->movie_id]) ?>">[View Movie]</a>
+                <a href="<?= Url::toRoute(['/site/movie', 'id' => $show->movie_id]) ?>">[Edit Movie]</a>
+                <a onclick="return confirm('Deleting this movie and all related showtimes?');"
+                   href="<?= Url::toRoute(['/site/delmovie', 'id' => $show->movie_id]) ?>">[Delete Movie]</a>
             </div>
             <div class="panel-body">
                 <span style="float: right">Show time: <?= $show->time . ":00" ?></span>
